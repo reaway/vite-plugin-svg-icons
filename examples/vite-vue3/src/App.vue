1d8486ec857a49cd5b800f5ac2bf52edac2ed9c7
reaway <reaway@foxmail.com>
2022-10-28 20:52:55 +0800

    init

---
+++ examples/vite-vue3/src/App.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+import SvgIcon from './components/SvgIcon.vue'
+</script>
+
+<template>
+  <SvgIcon name="image-load-fail"></SvgIcon>
+</template>
+
+<style scoped>
+
+</style>
---
+++ examples/vite-vue3/src/main.ts
@@ -0,0 +1,7 @@
+import { createApp } from 'vue'
+import App from './App.vue'
+
+// 加载 svg 图标
+import 'virtual:svg-icons-register'
+
+createApp(App).mount('#app')
